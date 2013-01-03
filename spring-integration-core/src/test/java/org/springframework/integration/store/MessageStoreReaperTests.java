935fe04f2ac786eb332f0c907ed1d8c2b4286bc8
Gary Russell <grussell@vmware.com>
2013-01-03 12:53:29 -0500

    INT-2830 Resolve Class Tangle
    MessageGroupStore <-> MessageGroupCallback
    
    Change callback to be an inner interface of the MGS.

--- spring-integration-core/src/test/java/org/springframework/integration/store/MessageStoreReaperTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/store/MessageStoreReaperTests.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2010 the original author or authors.
- * 
+ * Copyright 2002-2013 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -24,34 +24,36 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.integration.message.GenericMessage;
+import org.springframework.integration.store.MessageGroupStore.MessageGroupCallback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Dave Syer
  * @author Dave Turanski
+ * @author Gary Russell
  */
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MessageStoreReaperTests {
-	
+
 	@Autowired
 	@Qualifier("messageStore")
 	private MessageGroupStore messageStore;
-	
+
 	@Autowired
 	@Qualifier("messageStore2")
 	private MessageGroupStore messageStore2;
-	
+
 	@Autowired
 	@Qualifier("reaper2")
 	private MessageGroupStoreReaper reaper2;
-	
-	@Autowired 
+
+	@Autowired
 	@Qualifier("expiryCallback")
 	private ExpiryCallback expiryCallback;
-	
-	@Autowired 
+
+	@Autowired
 	@Qualifier("expiryCallback2")
 	private ExpiryCallback expiryCallback2;
 
@@ -64,33 +66,33 @@ public class MessageStoreReaperTests {
 		assertEquals(0, messageStore.getMessageGroup("FOO").size());
 		assertEquals(1, expiryCallback.groups.size());
 	}
-	
-	@Test 
+
+	@Test
 	public void testSmartLifecycle() throws Exception{
-		
+
 		messageStore2.addMessageToGroup("FOO", new GenericMessage<String>("foo"));
 		assertEquals(1, messageStore2.getMessageGroup("FOO").size());
-		
+
 		reaper2.setExpireOnDestroy(true);
 		reaper2.setTimeout(0);
-	
-		
+
+
 		if (!reaper2.isAutoStartup()){
 			reaper2.start();
 		}
-		
+
 		assertTrue(reaper2.isRunning());
-	
+
 		//reaper timeout is set to 0, but need to ensure positive elapsed time
 		Thread.sleep(1L);
-		
+
 		reaper2.stop();
 		assertTrue(!reaper2.isRunning());
-		
+
 		assertEquals(0, messageStore2.getMessageGroup("FOO").size());
 		assertEquals(1, expiryCallback2.groups.size());
 	}
-	
+
 	public static class ExpiryCallback implements MessageGroupCallback {
 
 		public final List<MessageGroup> groups = new ArrayList<MessageGroup>();
@@ -99,7 +101,7 @@ public class MessageStoreReaperTests {
 			groups.add(group);
 			messageGroupStore.removeMessageGroup(group.getGroupId());
 		}
-		
+
 	}
 
 }
